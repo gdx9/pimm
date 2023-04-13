0b0bd41b7f6449cee853a902ae7627ab369339c7
onthewaytootaku <onthewaytootaku@gmail.com>
2023-04-13 18:09:50 +0900

    add modifications for speed improvement

--- pimm_cpu/conversions.cpp
+++ pimm_cpu/conversions.cpp
@@ -4,25 +4,23 @@ using namespace std;
 
 namespace pimm{
 
-void cpu::rgb_to_gray_mean(uint8_t* rgb, uint8_t* gray, const size_t kSize){
-    for(size_t i = 0; i < kSize; ++i){
-        size_t rpos = i*3;
-        int32_t val = (static_cast<int32_t>(rgb[rpos + 0])
-            + static_cast<int32_t>(rgb[rpos + 1])
-            + static_cast<int32_t>(rgb[rpos + 2])) / 3;
+void cpu::rgb_to_gray_mean(uint8_t* rgb, uint8_t* gray, const size_t kSizeGray){
+    for(size_t pos_rgb = 0, pos_gray = 0; pos_gray < kSizeGray; pos_rgb += 3, ++pos_gray){
+        int32_t val = (static_cast<int32_t>(rgb[pos_rgb + 0])
+            + static_cast<int32_t>(rgb[pos_rgb + 1])
+            + static_cast<int32_t>(rgb[pos_rgb + 2])) / 3;
         // clip
-        gray[i] = static_cast<uint8_t>((val > 255) ? 255 : (val < 0) ? 0 : val);
+        gray[pos_gray] = static_cast<uint8_t>((val > 255) ? 255 : (val < 0) ? 0 : val);
     }
 }
 
-void cpu::rgb_to_gray_weighted(uint8_t* rgb, uint8_t* gray, const size_t kSize){
-    for(size_t i = 0; i < kSize; ++i){
-        size_t rpos = i*3;
-        float val = (static_cast<float>(rgb[rpos + 0]) * 0.299f
-            + static_cast<float>(rgb[rpos + 1]) * 0.587f
-            + static_cast<float>(rgb[rpos + 2]) * 0.144f);
+void cpu::rgb_to_gray_weighted(uint8_t* rgb, uint8_t* gray, const size_t kSizeGray){
+    for(size_t pos_rgb = 0, pos_gray = 0; pos_gray < kSizeGray; pos_rgb += 3, ++pos_gray){
+        float val = (static_cast<float>(rgb[pos_rgb + 0]) * 0.299f
+                    + static_cast<float>(rgb[pos_rgb + 1]) * 0.587f
+                    + static_cast<float>(rgb[pos_rgb + 2]) * 0.144f);
         // clip
-        gray[i] = static_cast<uint8_t>((val > 255.f) ? 255.f : (val < 0.f) ? 0.f : val);
+        gray[pos_gray] = static_cast<uint8_t>((val > 255.f) ? 255.f : (val < 0.f) ? 0.f : val);
     }
 }
 
@@ -41,25 +39,63 @@ void cpu::solarise_color(uint8_t* rgbImageSrc, uint8_t* rgbImageDst,
     }
 }
 
+uint8_t* prepare_gamma_lut_table(const float kGamma){
+    // prepare gamma lut table
+    // increases speed: from approximately 48 ms to 16 ms
+    const size_t kNumElements = 256;
+    uint8_t* gamma_lut_table = new uint8_t[kNumElements];
+
+    for(size_t i = 0; i < kNumElements; ++i){
+        float val = pow(static_cast<float>(i) / 255.f, kGamma) * 255.f;
+
+        // clip value
+        gamma_lut_table[i] = static_cast<uint8_t>(
+            (val < 0.f) ? 0.f : (val > 255.f) ? 255.f : val);
+    }
+
+    return gamma_lut_table;
+}
 void cpu::adjust_gamma(uint8_t* rgbImageSrc, uint8_t* rgbImageDst,
     const size_t kSize, const float kGamma){
+
+    const uint8_t* const gamma_lut_table = prepare_gamma_lut_table(kGamma);
+
+    // apply gamma to image
     for(size_t i = 0; i < kSize; ++i){
-        float val = pow((static_cast<float>(rgbImageSrc[i]) / 255.f), kGamma) * 255.f;
-        // clip value
-        rgbImageDst[i] = static_cast<uint8_t>((val < 0.f) ? 0.f : (val > 255.f) ? 255.f : val);
+        rgbImageDst[i] = gamma_lut_table[rgbImageSrc[i]];
     }
+
+    delete [] gamma_lut_table;
 }
 
-void cpu::adjust_contrast(uint8_t* rgbImageSrc, uint8_t* rgbImageDst,
-    const size_t kSize, const float kContrast){
+uint8_t* prepare_contrast_lut_table(const float kContrast){
+    // prepare contrast lut table
+    // increases speed: from approximately 15 ms to 8 ms
+    const size_t kNumElements = 256;
+    uint8_t* contrast_lut_table = new uint8_t[kNumElements];
+
     //F = 259*(255+contrast)/(255 *(259-contrast))
     const float kF = 259.f * (255.f + kContrast) / (255.f * (259.f - kContrast));
 
-    for(size_t i = 0; i < kSize; ++i){
-        float val = (kF * (static_cast<float>(rgbImageSrc[i]) - 128.f)) + 128.f;
+    for(size_t i = 0; i < kNumElements; ++i){
+        float val = (kF * static_cast<float>(i - 128.f)) + 128.f;
+
         // clip value
-        rgbImageDst[i] = static_cast<uint8_t>((val < 0.f) ? 0.f : (val > 255.f) ? 255.f : val);
+        contrast_lut_table[i] = static_cast<uint8_t>((val < 0.f) ? 0.f : (val > 255.f) ? 255.f : val);
     }
+
+    return contrast_lut_table;
+}
+void cpu::adjust_contrast(uint8_t* rgbImageSrc, uint8_t* rgbImageDst,
+    const size_t kSize, const float kContrast){
+
+    const uint8_t* const contrast_lut_table = prepare_contrast_lut_table(kContrast);
+
+    for(size_t i = 0; i < kSize; ++i){
+        rgbImageDst[i] = contrast_lut_table[rgbImageSrc[i]];
+    }
+
+    delete [] contrast_lut_table;
 }
 
 void cpu::adjust_brightness(uint8_t* rgbImageSrc, uint8_t* rgbImageDst,
